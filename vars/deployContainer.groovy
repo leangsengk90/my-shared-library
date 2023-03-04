a1ccb165440c0c0fce984696ee1295ec949e0988
leangsengk90 <48117906+leangsengk90@users.noreply.github.com>
2023-03-05 02:01:20 +0700

    Update deployContainer.groovy

--- vars/deployContainer.groovy
+++ vars/deployContainer.groovy
@@ -1,6 +1,5 @@
 #!/usr/bin/env groovy
-@Library('my-shared-library')
-import com.example.jenkins.shared.Deployer
+import src.com.example.jenkins.shared.Deployer
 
 def call(String imageTag, String containerName){
   Deployer.deployDockerContainer("${imageName}", "${imageTag}")
