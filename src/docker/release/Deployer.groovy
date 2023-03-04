e78d6d68c15db4cc4e1e747952d74dd8408f33e2
leangsengk90 <48117906+leangsengk90@users.noreply.github.com>
2023-03-05 03:13:57 +0700

    Update Deployer.groovy

--- src/docker/release/Deployer.groovy
+++ src/docker/release/Deployer.groovy
@@ -2,7 +2,7 @@ package docker.release
 
 class Deployer implements Serializable {
   static def deployDockerContainer(String imageTag, String containerName) {
-    sh "docker rm -f ${containerName} || true"
+    //sh "docker rm -f ${containerName} || true"
     sh "docker run -d -p 81:80 --name ${containerName} ${imageTag}"
   }
 }
