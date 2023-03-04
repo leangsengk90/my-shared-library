5276bb91adc33bdc1245c075bafb70aa86f13951
leangsengk90 <48117906+leangsengk90@users.noreply.github.com>
2023-03-05 01:03:01 +0700

    Update getResource.groovy

--- vars/getResource.groovy
+++ vars/getResource.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(String username){
-  def deploy = LibraryResource 'scripts/frontend/deploy.sh'
+  def deploy = libraryResource 'scripts/frontend/deploy.sh'
   writeFile(file: './deploy-script.sh', text: "${deploy}")
   sh "bash ./deploy-script.sh ${username}"
 }
