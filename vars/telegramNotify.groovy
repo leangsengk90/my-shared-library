6afb4a37679f5a9d6d277ce8d01ef1437d0a4bbd
leangsengk90 <48117906+leangsengk90@users.noreply.github.com>
2023-05-21 11:45:55 +0700

    Update telegramNotify.groovy

--- vars/telegramNotify.groovy
+++ vars/telegramNotify.groovy
@@ -1,4 +1,4 @@
 #!/usr/bin/env groovy
 def call(String msg){
-  sh 'curl -s -X POST https://api.telegram.org/bot5871480698:AAHy9EcjCMN4jUCjEUqYCx-f-KoZNVU4Uzg/sendMessage -d chat_id=456400937 -d text=${msg}' 
+  sh "curl -s -X POST https://api.telegram.org/bot5871480698:AAHy9EcjCMN4jUCjEUqYCx-f-KoZNVU4Uzg/sendMessage -d chat_id=456400937 -d text=${msg}" 
 }
